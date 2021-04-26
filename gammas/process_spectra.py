398196d0ec13c80e931102a7d30474bc5370d6df
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2021-04-26 11:04:47 -0400

    moving some stuff up higher in script in case there's research drive-related failures; also adding additional detector info

--- gammas/process_spectra.py
+++ gammas/process_spectra.py
@@ -26,28 +26,42 @@ def main():
                         help='string indicating a detector for which the spectra are being processed')
     args = parser.parse_args(sys.argv[1:])
     
-    detect_info = {'d1' : {'en_windows' : 'd1_hpge_energy_list_113.pkl', # this was created in a jupyter notebook using idx 88087
-                           'det_path' : 'd1_hpge/',
-                           'en_delta' : 2, # energy window is plus/minus 2 keV
+    # grab energy bins to create list of peaks training set
+    # created in en windows notebook using source activities of idx 88087
+    path = '/mnt/researchdrive/BOX_INTERNAL/opotowsky/detector_response/'
+    en_windows_fname = 'idx88087_energy_list_113.pkl'
+    with open(path + en_windows_fname, 'rb') as filehandle:
+        en_windows = pickle.load(filehandle)
+    # grab labels for the tracked peaks training set
+    actsdf = pd.read_pickle(path + 'nuc32_activities_scaled_1g_reindex.pkl')
+    lbls = ['ReactorType', 'CoolingTime', 'Enrichment', 'Burnup', 
+            'OrigenReactor', 'AvgPowerDensity', 'ModDensity', 'UiWeight'
+            ]
+    lbls_df = actsdf[lbls]
+
+    detect_info = {'d1' : {'det_path' : 'd1_hpge/',
+                           'en_delta' : 2,
                            'pkl_name' : 'd1_hpge_spectra_peaks_trainset.pkl.gz'
                            },
-                   'test':{'en_windows' : 'd1_hpge_energy_list_113.pkl', # this was created in a jupyter notebook using idx 88087
-                           'det_path' : 'd1_hpge/',
-                           'en_delta' : 2, # energy window is plus/minus 2 keV
-                           'pkl_name' : 'test.pkl.gz'
+                   'd2' : {'det_path' : 'd2_detective_hpge/',
+                           'en_delta' : 3,
+                           'pkl_name' : 'd2_hpge_spectra_peaks_trainset.pkl.gz'
+                           },
+                   'd3' : {'det_path' : 'd3_czt/',
+                           'en_delta' : 20,
+                           'pkl_name' : 'd3_czt_spectra_peaks_trainset.pkl.gz'
+                           },
+                   'd6' : {'det_path' : 'd6_sri2/',
+                           'en_delta' : 20,
+                           'pkl_name' : 'd6_sri2_spectra_peaks_trainset.pkl.gz'
                            },
                    }
     
-    path = '/mnt/researchdrive/BOX_INTERNAL/opotowsky/detector_response/'
     results_path = path + detect_info[args.detector]['det_path']
-    en_windows_fname = detect_info[args.detector]['en_windows']
-    with open(path + en_windows_fname, 'rb') as filehandle:
-        en_windows = pickle.load(filehandle)
-    
+    energy_bins = get_energy_bins(results_path + 'energy_bins.dat')
     en_delta = detect_info[args.detector]['en_delta']
     # output of `ls -1 | wc -l` in d1 directory is 5008 
     # need to exclude energy_bins.dat: range is 0 --> 5006(+1)
-    energy_bins = get_energy_bins(results_path + 'energy_bins.dat')
     for i in range(0, 5007):
     #for i in range(0,10): #for shorter train set
         gz = results_path + str(i) + '.dat.gz'
@@ -62,12 +76,6 @@ def main():
         else:
             peaksdf = peaksdf.append(windf)
     
-    # label the tracked peaks
-    actsdf = pd.read_pickle('nuc32_activities_scaled_1g_reindex.pkl')
-    lbls = ['ReactorType', 'CoolingTime', 'Enrichment', 'Burnup', 
-            'OrigenReactor', 'AvgPowerDensity', 'ModDensity', 'UiWeight'
-            ]
-    lbls_df = actsdf[lbls]
     #lbls_df = actsdf.iloc[0:peaksdf.index.tolist()[-1]+1][lbls] #for making shorter train set
     labeled_peaksdf = pd.concat([lbls_df, peaksdf], axis=1)
     labeled_peaksdf.to_pickle(path + detect_info[args.detector]['pkl_name'], compression='gzip')
