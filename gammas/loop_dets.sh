ae136faf80febd615275456a2ab3db030e04b5d8
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2021-05-14 11:26:44 -0400

    have params for looping process_spectra.py ready for next trainset creation once new energy window list is created

--- gammas/loop_dets.sh
+++ gammas/loop_dets.sh
@@ -6,8 +6,6 @@
 # params_process_spectra.txt. 
 
 # already done
-# d1_hpge/,idx88087_energy_list_113.pkl,2,8192,d1_hpge_spectra_xxpeaks_trainset.pkl.gz
-# d2_detective_hpge/,idx88087_energy_list_113.pkl,3,8192,d2_hpge_spectra_xxpeaks_trainset.pkl.gz
 
 while read p; do
     time ./process_spectra.py $p
