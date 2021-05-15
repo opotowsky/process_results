7bf5bd910b1d5e6a05d58b2766c48c5557a4e197
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2021-05-15 19:53:01 -0400

    have energy lists from auto peak finding created for first three detectors

--- gammas/loop_dets.sh
+++ gammas/loop_dets.sh
@@ -7,6 +7,11 @@
 
 # already done
 
+# not yet done
+#d4_nai/ idxYY_energy_list_xx.pkl 12 1024 d4_nai_spectra_xxpeaks_trainset.pkl.gz
+#d5_labr3/ idxYY_energy_list_xx.pkl 12 1024 d5_labr3_spectra_xxpeaks_trainset.pkl.gz
+#d6_sri2/ idxYY_energy_list_xx.pkl 10 1024 d6_sri2_spectra_xxpeaks_trainset.pkl.gz
+
 while read p; do
     time ./process_spectra.py $p
 done <params_process_spectra.txt
