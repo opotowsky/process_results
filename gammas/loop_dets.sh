2cfc288e7f4b942fdf68bcf2ffb29c7f4e51177b
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2021-05-27 16:38:56 -0400

    need to rerun process spectra for d2 auto. somehow d3 auto list got saved as d2.

--- gammas/loop_dets.sh
+++ gammas/loop_dets.sh
@@ -7,7 +7,10 @@
 
 # already done
 #d1_hpge/ idx88087_energy_list_d1auto.pkl 2 8192 d1_hpge_spectra_auto_peaks_trainset.pkl.gz
-#d2_detective_hpge/ idx88087_energy_list_d2auto.pkl 3 8192 d2_hpge_spectra_auto_peaks_trainset.pkl.gz
+#d3_czt/ idx88087_energy_list_d3auto.pkl 8 1024 d3_czt_spectra_auto_peaks_trainset.pkl.gz
+#d4_nai/ idx88087_energy_list_d4auto.pkl 12 1024 d4_nai_spectra_auto_peaks_trainset.pkl.gz
+#d5_labr3/ idx88087_energy_list_d5auto.pkl 12 1024 d5_labr3_spectra_auto_peaks_trainset.pkl.gz
+#d6_sri2/ idx88087_energy_list_d6auto.pkl 10 1024 d6_sri2_spectra_auto_peaks_trainset.pkl.gz
 
 # not yet done
 
